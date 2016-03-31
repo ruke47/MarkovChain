5f4fb767a1e5038f3d48a0552ddef117038303ce
Andrew Rueckert <andrew@praesid.io>
2016-03-31 11:55:25 -0700

    Remove all dependencies.
    I don't REALLY need ImmutableMap.of()...

--- src/main/java/com/bigdumbmustache/markovchain/Main.java
+++ src/main/java/com/bigdumbmustache/markovchain/Main.java
@@ -1,18 +1,16 @@
 package com.bigdumbmustache.markovchain;
 
-import com.google.common.base.Throwables;
-import com.google.common.collect.ImmutableMap;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    private static final Map<String, Integer> corpus = ImmutableMap.of(
-            "ospd.txt", 1
-    );
+    private static final Map<String, Integer> corpus = new HashMap<String, Integer>() {{
+        put("ospd.txt", 1);
+    }};
 
     public static void main(String... args) throws IOException {
         final Chain chain = new Chain(4);
@@ -21,7 +19,7 @@ public class Main {
             try (final BufferedReader reader = resource(name)) {
                 chain.learn(reader, weight);
             } catch (IOException e) {
-                throw Throwables.propagate(e);
+                throw new RuntimeException(e);
             }
 
         });
--- src/main/java/com/bigdumbmustache/markovchain/Node.java
+++ src/main/java/com/bigdumbmustache/markovchain/Node.java
@@ -1,12 +1,11 @@
 package com.bigdumbmustache.markovchain;
 
-import org.apache.commons.math3.random.RandomDataGenerator;
-
 import java.util.Map;
+import java.util.Random;
 import java.util.TreeMap;
 
 public class Node<T> {
-    private static final RandomDataGenerator random = new RandomDataGenerator();
+    private static final Random random = new Random();
     Map<T, Integer> nextChars = new TreeMap<>();
     transient int totalWeight = 0;
 
@@ -17,7 +16,7 @@ public class Node<T> {
     }
 
     public T pickChar() {
-        final int n = random.nextInt(0, totalWeight);
+        final int n = random.nextInt(totalWeight+1);
         int sum = 0;
         for (Map.Entry<T, Integer> entry : nextChars.entrySet()) {
             sum += entry.getValue();
